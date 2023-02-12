6add436e4f09d55c5f145c3a9350dff5d85aff99
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-12 10:18:51 +0100

    create Details, ExerciseVedios and SimilarExercises components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import React,{useState,useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import {exerciseOptions,fetchData} from '../util/fetchData'
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  return
+  <Box>
+    <Details/>
+    <ExersiceVedios/>
+    <SimilarExercises/>
+  </Box>
 };
 
 export default ExerciseDetail;
